5626690c5729a7680e8383f3f038a623f7767c6b
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2023-10-06 10:37:36 +0530

    run animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,53 @@ boy.src = "assets/images/png/idle_("+ idleImageNumber +").png";
 function idleAnimationStart(){
      idleAnimationNumber = setInterval(idleAnimation,200);
  }
+runImageNumber =0;
+runAnimationNumber = 0;
+
+function runAnimation(){
+
+    runImageNumber = runImageNumber + 1;
+
+    if (runImageNumber == 11) {
+        runImageNumber = 1;
+    }
+
+    boy.src = "assets/images/png/Run (" + runImageNumber + ").png";
+}
+
+function runAnimationStart(){
+    runAnimationNumber = setInterval(runAnimation,100);
+    clearInterval(idleAnimationNumber);
+
+}
+
+function keyCheck(event){
+    // alert(event.which);
+    // enter = 13
+
+    var keyCode = event.which;
+    if (keyCode == 13){
+        if(runAnimationStart ==0){
+
+            runAnimationStart();
+
+        }
+    }
+
+    if(moveBackgroundAnimationId == 0){
+        moveBackgroundAnimationId = setInterval(moveBackground,100);
+
+    }
+}
+
+var backgroundImagePositionX = 0;
+var moveBackgroundAnimationId = 0;
+
+
+function moveBackground(){
+
+    backgroundImagePositionX = backgroundImagePositionX - 20;
+
+    document.getElementById("background").style.backgroundPositionX = backgroundImagePositionX + "px";
+
+}
