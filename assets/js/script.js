e0d5aa2ecd7b78e1ea97cf33fe34f5d20a0cc1b2
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2023-10-07 17:59:19 +0530

    score

--- assets/js/script.js
+++ assets/js/script.js
@@ -137,12 +137,21 @@ function keyCheck(event) {
 var backgroundImagePositionX = 0;
 var moveBackgroundAnimationId = 0;
 
+//-----------------score-----------------------
+
+var score =0;
+
+// ---------------------
 
 function moveBackground(){
 
     backgroundImagePositionX = backgroundImagePositionX - 20;
 
     document.getElementById("background").style.backgroundPositionX = backgroundImagePositionX + "px";
+
+
+    score = score +1;  // score
+    document.getElementById("score").innerHTML = score;
 
 }
 
